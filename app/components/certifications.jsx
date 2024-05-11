00c65b89c1f9f898febe91048e4a807f6f3dae9d
azwadmirza <mirzaazwad@iut-dhaka.edu>
2024-05-11 21:35:32 +0600

    Publish to Pages Bug Fix #13

--- app/components/certifications.jsx
+++ app/components/certifications.jsx
@@ -13,7 +13,7 @@ const Certifications = () => {
                 <div className="w-4/5">
                     <h1 className="text-2xl mb-2">Machine Learning Specialization</h1>
                     <p className="text-xl">By DeepLearning.ai</p>
-                    <Image src="/certificate.jpeg" alt="ICT Fest 2024 By IUT CSE" width={500} height={300} className="m-4 px-4 py-2 border border-white rounded-lg" />
+                    <Image src="/dist/certificate.jpeg" alt="ICT Fest 2024 By IUT CSE" width={500} height={300} className="m-4 px-4 py-2 border border-white rounded-lg" />
                 </div>
             </div>
         </Card.Body>
---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"s5qQbud+A0ibC1R7AsIyHiryRXpTiqb8pJGZjfHtLoI=\"\n}"
