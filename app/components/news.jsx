f6e6c6eaaec5b424fa9dd713b6613a612e6a5452
azwadmirza <mirzaazwad@iut-dhaka.edu>
2024-05-13 17:52:14 +0600

    News Feature responsive

--- app/components/news.jsx
+++ app/components/news.jsx
@@ -2,22 +2,18 @@
 import { Card } from "react-bootstrap";
 
 const News = () => {
-    return ( 
+    return (
         <Card className="w-full bg-zinc-950 px-4 py-2 rounded-lg">
             <Card.Header className="text-4xl w-full mb-6 text-left">News</Card.Header>
             <Card.Body>
-                <div className="w-full flex">
-                    <div className="w-1/5 ms-4 me-4">
-                        <p className="text-xl">5<sup>th</sup> May, 2024</p>
-                    </div>
-                    <div className="w-4/5">
-                        <h1 className="text-2xl">Intenship Alert</h1>
-                        <p className="text-xl">I have recently received an intenship oppurtunity to work at <a href="https://bangladesh.ai/">Intelligent Machines, Bangladesh</a></p>
-                    </div>
+                <div className="w-full flex flex-col">
+                    <p className="text-sm text-gray-400">5<sup>th</sup> May, 2024</p>
+                    <h1 className="text-lg md:text-2xl">Intenship Alert</h1>
+                    <p className="text-md md:text-xl">I have recently received an intenship oppurtunity to work at <a href="https://bangladesh.ai/">Intelligent Machines, Bangladesh</a></p>
                 </div>
             </Card.Body>
         </Card>
     );
 }
- 
+
 export default News;
