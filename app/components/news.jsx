cd6f885951a1effc530abd19a2ea1025253acb34
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-10-07 15:44:09 +0600

    Update news.jsx

--- app/components/news.jsx
+++ app/components/news.jsx
@@ -11,6 +11,11 @@ const News = () => {
                     <h1 className="text-lg md:text-2xl">Intenship Alert</h1>
                     <p className="text-md md:text-xl">I have recently received an intenship oppurtunity to work at <a href="https://bangladesh.ai/">Intelligent Machines, Bangladesh</a></p>
                 </div>
+                <div className="w-full flex flex-col">
+                    <p className="text-sm text-gray-400">5<sup>th</sup> Oct, 2024</p>
+                    <h1 className="text-lg md:text-2xl">Job Alert</h1>
+                    <p className="text-md md:text-xl">I have recently received a part-time job oppurtunity to work at <a href="https://bangladesh.ai/">Intelligent Machines, Bangladesh</a> as a part of the product-engineering team</p>
+                </div>
             </Card.Body>
         </Card>
     );
