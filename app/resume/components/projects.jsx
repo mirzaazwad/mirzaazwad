d428a3a4488174b75d2ee5000393e93d76e4fd93
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-10-07 15:52:06 +0600

    Update projects.jsx

--- app/resume/components/projects.jsx
+++ app/resume/components/projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <div className="projects">
 
             <div className="subrubric">
-                <Link href="https://github.com/azwadmirza"><h1 className="text-xl mb-2 hover:text-black hover:bg-white px-4 py-2 rounded-lg">Academic Projects <IonIcon icon={logoGithub}></IonIcon></h1></Link>
+                <Link href="https://github.com/mirzaazwad"><h1 className="text-xl mb-2 hover:text-black hover:bg-white px-4 py-2 rounded-lg">Projects <IonIcon icon={logoGithub}></IonIcon></h1></Link>
                 <div className="flex flex-col mb-2 px-4 py-2 rounded-lg bg-black">
                     <div className="w-1/5 font-bold">CSE 4554-Machine Learning</div>
                     <div>Titled: <strong>Automated Python Code Bug Detection using Software Metrics</strong>, employing software metrics and ML to predict bugs in Python code.</div>
@@ -28,10 +28,6 @@ const Projects = () => {
                     <div className="w-1/5 font-bold">SWE 4504-Design Project Lab I</div>
                     <div>Titled: <strong>DataAnalytica.io</strong>, built with React, Python, Django, and MongoDB for Exploratory Data Analysis and basic ML simulations.</div>
                 </div>
-            </div>
-
-            <div className="subrubric">
-            <Link href="https://github.com/mirzaazwad"><h1 className="text-xl mb-2 hover:text-black hover:bg-white px-4 py-2 rounded-lg">Personal and Professional Projects <IonIcon icon={logoGithub}></IonIcon></h1></Link>
                 <div className="flex flex-col mb-2 px-4 py-2 rounded-lg bg-black">
                     <div className="w-1/5 font-bold">Vaccine App</div>
                     <div>Developed with Google Cloud Run and Cloud Build, logging with DataDog. <strong>(BUET CSE Fest Hackathon 2023-DevOps Segment)</strong></div>
